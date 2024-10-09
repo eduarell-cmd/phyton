f8b7c4a3cb46bddbc4f4c1f4a0511f9f26b80d2a
EduardoA <edu.arellanes9@gmail.com>
2024-10-08 22:25:26 -0600

    cambio if

--- media.py
+++ media.py
@@ -1,9 +1,15 @@
-jose = [10,10,9,10,8];
+jose = [9,10,9,10,8];
 maria = [10,10,9,8,10];
 
 def mean(numbers):
     return sum(numbers) / len(numbers)
 jose=mean(jose)
 maria=mean(maria)
-print (f'Media de jose: {jose:.1f}')
-print (f'Media de maria: {maria:.1f}')
+if jose>maria:
+    print (f'Media de jose: {jose:.1f}')
+    print('Maria se la pela a jose')
+elif jose==maria:
+    print ('Nunguno de los dos vlv')
+else:
+    print (f'Media de maria: {maria:.1f}')
+    print('jose se la pela a Maria')
