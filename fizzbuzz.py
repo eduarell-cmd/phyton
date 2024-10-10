024f296aa54dea2f1a2aa079ff57fea6c2969f5d
EduardoA <edu.arellanes9@gmail.com>
2024-10-09 20:27:34 -0600

    .

---
+++ fizzbuzz.py
@@ -0,0 +1,9 @@
+for (x) in range (1,101):
+    j = x%3 == 0
+    k = x%5 == 0
+    if k == j:
+        print ('FizzBuzz')
+    elif (j) :
+        print ('Fizz')
+    else :
+        print ('Buzz') 
